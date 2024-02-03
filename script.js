bbf6c97c7845c9c7d83b892eb4b6da7b0b28e46e
Łukasz Sadowski <Lukasz.sadowski2605@gmail.com>
2024-02-03 09:18:52 +0100

    added costs

--- script.js
+++ script.js
@@ -2,11 +2,22 @@
 const navigationBar = document.querySelector(".navigation-bar");
 const navigationMobile = document.querySelector(".navigation");
 const closeMobileMenu = document.querySelector(".close-btn");
+const carouselItem = document.querySelectorAll(".carousel-item");
 
 const changeActiveMenu = () => {
   navigationBar.classList.toggle("active");
   navigationMobile.classList.toggle("active");
 };
+
+const addActiveToCarouselItem = () => {
+  if (window.innerWidth >= 1200) {
+    carouselItem.forEach((item) => item.classList.add("active"));
+  } else if (window.innerWidth < 1200) {
+    carouselItem.forEach((item) => item.classList.remove("active"));
+  }
+};
+
+addActiveToCarouselItem();
 
 navigationBar.addEventListener("click", changeActiveMenu);
 closeMobileMenu.addEventListener("click", changeActiveMenu);
